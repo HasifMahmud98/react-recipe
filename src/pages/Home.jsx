4282713a40175dfb630527f6c337c41c7d4c6928
HasifMahmud98 <hasifmahmud98@gmail.com>
2022-03-18 04:26:11 +0600

    React Recipe v1.0

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+import Veggie from '../components/Veggie';
+import Popular from '../components/Popular';
+
+function home() {
+  return (
+    <div>
+        <Veggie></Veggie>
+        <Popular></Popular>
+    </div>
+  )
+}
+
+export default home
